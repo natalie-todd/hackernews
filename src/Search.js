57c725ac9c2ebbfb7757481ab976c599cbc73a59
natalie-todd <natalie.todd15@gmail.com>
2019-06-09 12:00:42 -0600

    add css

---
+++ src/Search.js
@@ -0,0 +1,14 @@
+class Search extends Component {
+    render() {
+        const { value, onChange } = this.props;
+        return (
+            <form>
+                <input 
+                type="text"
+                value={value}
+                onChange={onChange}
+                />
+            </form>
+        )
+    }
+}
