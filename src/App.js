d29556224ce01a1c3b50ee1a1407fb780a7b5e97
natalie-todd <natalie.todd15@gmail.com>
2019-06-09 16:17:31 -0600

    Add fetch

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const DEFAULT_QUERY = 'redux';
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+
+const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
+console.log(url);
+
 const largeColumn = {
   width: '40%',
-  };
-  const midColumn = {
+};
+const midColumn = {
   width: '30%',
-  };
-  const smallColumn = {
+};
+const smallColumn = {
   width: '10%',
-  };
+};
 
 const list = [
   {
@@ -38,12 +46,26 @@ class App extends Component {
     super(props);
 
     this.state = {
-      list,
-      searchTerm: '',
+      result: null,
+      searchTerm: DEFAULT_QUERY,
     };
 
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  setSearchTopStories(result) {
+    this.setState({ result });
+  }
+
+  componentDidMount() {
+    const { searchTerm } = this.state;
+
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(error => error);
   }
 
   onSearchChange(event) {
@@ -57,7 +79,9 @@ class App extends Component {
   }
 
   render() {
-    const { searchTerm, list } = this.state;
+    console.log(this.state)
+    const { searchTerm, result } = this.state;
+    if (!result) { return null; }
     return (
       <div className="page">
         <div className="interactions">
@@ -69,7 +93,7 @@ class App extends Component {
         </Search>
         </div>
         <Table
-          list={list}
+          list={result.hits}
           pattern={searchTerm}
           onDismiss={this.onDismiss}
         />
@@ -94,16 +118,16 @@ const Table = ({ list, pattern, onDismiss }) =>
         <span style={{ largeColumn }}>
           <a href={item.url}>{item.title}</a>
         </span>
-        <span style={ midColumn }>
+        <span style={midColumn}>
           {item.author}
         </span>
-        <span style={ smallColumn }>
+        <span style={smallColumn}>
           {item.num_comments}
         </span>
-        <span style={ smallColumn }>
+        <span style={smallColumn}>
           {item.points}
         </span>
-        <span style={ smallColumn }>
+        <span style={smallColumn}>
           <button
             onClick={() => onDismiss(item.objectID)}
             className="button-inline"
